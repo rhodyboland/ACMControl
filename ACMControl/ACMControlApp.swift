73881438538b476210aac0965ec5d447beae8450
Rhody Boland <rhodyboland@hotmail.com>
2024-09-26 17:24:14 +1000

    Initial Commit

---
+++ ACMControl/ACMControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACMControlApp.swift
+//  ACMControl
+//
+//  Created by Rhody Boland on 26/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct ACMControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
